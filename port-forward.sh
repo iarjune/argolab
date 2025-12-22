981fba2f81e1728ed78aab9907110952e5ead0e3
Ivan A <iarjune@admarketplace.com>
2025-12-22 03:11:38 -0500

    sync

--- port-forward.sh
+++ port-forward.sh
@@ -2,4 +2,3 @@ kubectl -n argo port-forward svc/argo-server 2746:2746 &>/dev/null &
 kubectl -n argocd port-forward svc/argocd-server 8888:80 &>/dev/null &
 kubectl -n argocd port-forward svc/argocd-server 8443:443 &>/dev/null &
 kubectl -n kargo port-forward svc/kargo-api 31444:443 &>/dev/null &
-
