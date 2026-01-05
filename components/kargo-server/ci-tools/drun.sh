fc3a5939ed4fc20ce3c05d9e0bfd297c2d934864
Ivan A <iarjune@admarketplace.com>
2026-01-04 21:53:43 -0500

    refactor

--- components/kargo-server/ci-tools/drun.sh
+++
@@ -1,8 +0,0 @@
-docker run --rm \
-  -e APP=ci-test-python-project \
-  -e TAG=1.12.182 \
-  -e STAGE=dev \
-  -e KARGO_SERVER_URL="https://kargo.ric1.admarketplace.net" \
-  -e KARGO_PW="iIUSN4aAGVeT83odP8nb3hTI" \
-  -e KARGO_PROJECT=ci-test-python-project\
-  kargo-runner
